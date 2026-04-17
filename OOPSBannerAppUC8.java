dc9c98a0d461a137adc2adbeef9c345fbd29319c
meghna-098 <mv7778@srmist.edu.in>
2026-04-17 22:06:11 +0530

    Coded UC8: Display Hello World in console

---
+++ OOPSBannerAppUC8.java
@@ -0,0 +1,97 @@
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class OOPSBannerAppUC8 {
+
+    /**
+     * Utility method to build and return character patterns map
+     * 
+     * @return Map of character to pattern
+     */
+    public static Map<Character, String[]> buildCharacterPatterns() {
+
+        Map<Character, String[]> map = new HashMap<>();
+
+        // Pattern for O
+        map.put('O', new String[]{
+            " ***** ",
+            "*     *",
+            "*     *",
+            "*     *",
+            "*     *",
+            "*     *",
+            " ***** "
+        });
+
+        // Pattern for P
+        map.put('P', new String[]{
+            "****** ",
+            "*     *",
+            "*     *",
+            "****** ",
+            "*      ",
+            "*      ",
+            "*      "
+        });
+
+        // Pattern for S
+        map.put('S', new String[]{
+            " ***** ",
+            "*     *",
+            "*      ",
+            " ***** ",
+            "      *",
+            "*     *",
+            " ***** "
+        });
+
+        return map;
+    }
+
+    /**
+     * Utility method to get pattern for a character
+     * 
+     * @param ch input character
+     * @param map character pattern map
+     * @return pattern array
+     */
+    public static String[] getCharacterPattern(char ch, Map<Character, String[]> map) {
+        return map.getOrDefault(ch, new String[]{
+            " ", " ", " ", " ", " ", " ", " "
+        });
+    }
+
+    /**
+     * Utility method to print banner
+     * 
+     * @param text input word
+     * @param map character pattern map
+     */
+    public static void printBanner(String text, Map<Character, String[]> map) {
+
+        for (int i = 0; i < 7; i++) {
+            StringBuilder line = new StringBuilder();
+
+            for (char ch : text.toCharArray()) {
+                String[] pattern = getCharacterPattern(ch, map);
+                line.append(pattern[i]).append("  ");
+            }
+
+            System.out.println(line);
+        }
+    }
+
+    /**
+     * Main Method
+     */
+    public static void main(String[] args) {
+
+        // Build Map
+        Map<Character, String[]> patternMap = buildCharacterPatterns();
+
+        // Print Banner
+        printBanner("OOPS", patternMap);
+    }
+}
